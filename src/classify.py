a30ba7818e99f32a2a3147c5cdf723a147396120
yhc615 <annochang@gmail.com>
2018-03-16 15:06:17 +0000

    added files, work documented on jupyter notebook

---
+++ src/classify.py
@@ -0,0 +1,81 @@
+import sys
+from sklearn.multiclass import OneVsRestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn import linear_model
+from sklearn import svm
+from sklearn import metrics
+import numpy as np
+
+from src.utils.data_utils import *
+
+def print_metrics(classifier,X_in,Y_in,X_out,Y_out,preds_in,preds_out,verbose=False):
+	class_names = ["Worst","Worse","Average","Better","Best"]
+	if verbose:
+		print("Confusion Matrix(Training Sample):\n")
+		cnf_matrix = metrics.confusion_matrix(Y_in, preds_in)
+		np.set_printoptions(precision=2)
+		# Plot non-normalized confusion matrix
+		plt.figure()
+		plot_confusion_matrix(cnf_matrix, classes=class_names,
+                      title='Confusion matrix, without normalization')
+		# Plot normalized confusion matrix
+		plt.figure()
+		plot_confusion_matrix(cnf_matrix, classes=class_names, normalize=True,
+                      title='Normalized confusion matrix')
+		plt.show()
+
+		print('Mean squared error for training data:', metrics.mean_squared_error(Y_in, preds_in))	
+		print("Training set score(sklearn):",classifier.score(X_in,Y_in))
+		print("\nClassification Report(Training set):\n\n",metrics.classification_report(Y_in,preds_in))
+		print("\n")
+	
+	print("Confusion Matrix(Testing Sample):\n")
+	cnf_matrix = metrics.confusion_matrix(Y_out, preds_out)
+	np.set_printoptions(precision=2)
+	# Plot non-normalized confusion matrix
+	plt.figure()
+	plot_confusion_matrix(cnf_matrix, classes=class_names,
+                      title='Confusion matrix, without normalization')
+	# Plot normalized confusion matrix
+	plt.figure()
+	plot_confusion_matrix(cnf_matrix, classes=class_names, normalize=True,
+                      title='Normalized confusion matrix')
+	plt.show()
+	print("\nConfusion Matrix(Test Sample):\n\n",metrics.confusion_matrix(Y_out, preds_out))#Confusion Matrix	
+	print('Mean squared error for test data:', metrics.mean_squared_error(Y_out, preds_out))
+	print("Test set score(sklearn):",classifier.score(X_out,Y_out))#score meaning depends on classifier
+	print("Classification Report(Training set):\n",metrics.classification_report(Y_out,preds_out))
+
+def classify(datapath,v):#datapath: directory name of the datasets, (v)erbose: True or false 
+	#Grab both wine datasets in one dataset
+	concat_data = get_data(datapath)
+	#Bag data to 5 scores 
+	recode = {3:0, 4:0, 5:1, 6:2, 7:3, 8:4,9:4}
+	concat_data['quality_c'] = bag_data(recode,concat_data,'quality')
+
+	#Split up dataset 70/30 training,testing
+	y_wine = concat_data['quality_c']
+	X_wine = concat_data.drop(['quality_c','quality'], axis=1)
+	X_train, X_test, y_train, y_test = train_test_split(X_wine, y_wine, test_size=0.2, random_state=420)
+	#==========================================================================================
+	#Linear Regression Estimaro, OnevsRest classification
+	clf = OneVsRestClassifier(estimator=linear_model.LinearRegression())
+	clf.fit(X_train, y_train)
+	#Make Predictions for both sets
+	pred_train = clf.predict(X_train)
+	pred_test = clf.predict(X_test)
+	print('='*30+"\nLinear Regression:\n")
+	print_metrics(clf,X_train,y_train,X_test,y_test,pred_train,pred_test,verbose=v)
+	#==========================================================================================
+	#Support Vector Estimator, OnevsRest classification
+	clf = OneVsRestClassifier(estimator=svm.SVC(kernel='rbf',random_state=0))
+	clf.fit(X_train, y_train)
+	#Make Predictions for both sets
+	pred_train = clf.predict(X_train)
+	pred_test = clf.predict(X_test)
+	print('='*30+"\nSVC :\n")
+	print_metrics(clf,X_train,y_train,X_test,y_test,pred_train,pred_test,verbose=v)
+	#==========================================================================================
+
+if __name__ == '__main__':
+	classify(str(sys.argv[1]),bool(sys.argv[2]))
---
+++ src/utils/data_utils.py
@@ -0,0 +1,75 @@
+import pandas as pd
+import numpy as np
+import itertools
+import matplotlib.pyplot as plt
+
+
+
+def get_data(datdir):
+	#get_data  : Grabs entire dataset from given dataset directory name, 'datdir' 
+	#oututs tuple of red wine and white wine data
+	#Load red and white wine datasets (; separated)
+	red_dat = pd.read_csv( "./" + datdir + "/winequality-red.csv",';')
+	white_dat = pd.read_csv( "./" + datdir + "/winequality-white.csv", ';')
+	out = [red_dat, white_dat]
+	out = pd.concat(out)
+	return out
+
+def bag_data(map,data,attr):
+	#sklearn stub for data recoding(bagging)
+	return data[attr].map(map)
+
+def input_training_set(inp):
+	#create input functions for tf
+	features = {}
+	for attr in inp.columns.values:
+		features[attr] = inp[attr].as_matrix()
+	labels = inp['quality'].as_matrix()
+	return features,labels
+
+
+def one_hot_label(labels):
+	#converts target values into one hot encoding
+	out = np.array([])
+	for val in labels:
+		row = np.zeros(9)
+		print (val)
+		row[val-1] = 1
+		out = np.append(out,row,axis=0)
+	return out
+
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+    """
+    COPYRIGHT SKLEARN : http://scikit-learn.org/stable/auto_examples/model_selection/plot_confusion_matrix.html
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    print(cm)
+
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt),
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+
